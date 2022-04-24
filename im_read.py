bc9717c8be545bc570c043277bf847550d413164
Bharat Kesari <51207521+bkesari1998@users.noreply.github.com>
2022-04-23 22:23:02 -0400

    Update im_read.py

--- im_read.py
+++ im_read.py
@@ -13,12 +13,12 @@ def image_callback(img_msg):
     img = img_msg
 
 # Initalize a subscriber to the "/camera/rgb/image_raw" topic with the function "image_callback" as a callback
-def publish_img():
+def im_read():
     rospy.init_node('im_read', anonymous=True)
     sub = rospy.Subscriber('/camera/rgb/image_raw', Image, image_callback)
 
 
 # Loop to keep the program from shutting down unless ROS is shut down, or CTRL+C is pressed
 while not rospy.is_shutdown():
-    publish_img()
+    im_read()
     rate.sleep()
